a9eb69b2fc0c21b74735447df7e229724aaa8edd
Rodrigo Lima <digo100200@gmail.com>
2020-05-27 23:42:55 -0300

    apagando classes nao usadas

--- src/main/java/com/courses/repositories/CurriculumRepository.java
+++
@@ -1,7 +0,0 @@
-package com.courses.repositories;
-
-import com.courses.entities.Curriculum;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CurriculumRepository extends JpaRepository<Curriculum, Integer> {
-}
--- src/main/java/com/courses/repositories/SubjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.courses.repositories;
-
-import com.courses.entities.Subject;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubjectRepository extends JpaRepository<Subject, Integer> {
-}
